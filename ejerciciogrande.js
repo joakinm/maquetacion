c9ef017681b1a4872e086965a2df06538fab8aaf
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2020-04-13 15:00:51 -0300

    ejercicios de maquetacion html y cs

---
+++ ejerciciogrande.js
@@ -0,0 +1,12 @@
+function ocultar3cols()
+{
+    document.getElementById("trescolumnas").style.display = "none";
+}
+function ocultarcentro()
+{
+    document.getElementById("contenedorgrande").style.display = "none";
+}
+function ocultar2cols()
+{
+    document.getElementById("coldespegadas2").style.display = "none";
+}
